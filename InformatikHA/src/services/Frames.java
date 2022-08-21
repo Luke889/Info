98e60a47a377b755aed0f7a792c2186c40c492ec
Luhe_Xx07_ <79638088+Luke889@users.noreply.github.com>
2022-08-21 19:31:17 +0200

    Add files via upload

---
+++ InformatikHA/src/services/Frames.java
@@ -0,0 +1,21 @@
+package services;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Frames {
+    public static class gui extends JFrame implements ActionListener{
+
+        public gui(){
+
+        }
+        public static void main(String[] args){
+
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
